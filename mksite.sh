80cc023a699ee3033d165a25fb4fd135c9c388b8
Serra Aydin <serra.aydin@oaknorth.ai>
2019-07-17 12:50:50 +0300

    added dev file checks

--- mksite.sh
+++ mksite.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+shopt -s extglob
 set -e
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
@@ -9,13 +10,20 @@ if [ $# -eq 0 ]
   exit 1
 fi
 
+PID=$(ps aux | grep '/opt/tljh/user/bin/python3.6 /opt/tljh/user/bin/mkdocs' | awk '{print $2}')
+
+if [ ! -z "$PID" ]
+  then
+    sudo kill $PID
+fi
+
 cd /srv/mksite
 
 if [ ! -d "$1" ]
-then
+  then
     sudo git clone /srv/repo/$1.git
 fi
-
+                                                                                                                                                                                                 1,1           Top
 cd $1
 
 sudo git checkout develop
@@ -24,17 +32,21 @@ sudo git pull origin develop
 if [ -d ./serve/docs ]; then
    sudo rm -r ./serve/docs
 fi
+
 sudo mkdir -p ./serve/docs
 sudo mkdir -p ./serve/docs/images
 sudo mkdir -p ./serve/docs/stylesheets
+
 sudo cp ../mkdocs.yml ./serve/
-sudo cp *.ipynb ./serve/docs/
+sudo cp !(__*).ipynb ./serve/docs/
+
 cd ./serve/docs
 sudo jupyter nbconvert --to markdown --TemplateExporter.exclude_code_cell=False --TemplateExporter.exclude_input=True *.ipynb
 sudo rm *.ipynb
+
 cd ..
 sudo cp /srv/tpl/images/logo.svg docs/images/
-sudo cp /srv/tpl/images/favicon.ico docs/images/
+sudo cp /srv/tpl/images/favicon.ico docs/images/                                                                                                                                                                                                 43,1          88%
 sudo cp /srv/tpl/oaknorth.css docs/stylesheets/
-#sudo mkdocs build
+
 sudo mkdocs serve
